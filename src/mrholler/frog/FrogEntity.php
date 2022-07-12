69c8888922493a2d8cfae05a19696397673b7f6a
mrholler228@gmail.com <mrholler228@gmail.com>
2022-07-13 01:24:36 +0300

    implemented attack entity

--- src/mrholler/frog/FrogEntity.php
+++ src/mrholler/frog/FrogEntity.php
@@ -8,6 +8,9 @@ use pocketmine\entity\Living;
 use pocketmine\player\Player;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\entity\EntitySizeInfo;
+use pocketmine\entity\Entity;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 
 class FrogEntity extends Living{
 
@@ -18,30 +21,31 @@ class FrogEntity extends Living{
 	private array $inventory = [];
 	private string $nametag = "Frog";
 	private ?Player $player = null;
+	private bool $isAttacking = false;
+	private ?Entity $target = null;
 
-	public static function getNetworkTypeId() : string{ return "minecraft:frog"; }
+	public static function getNetworkTypeId() : string { return "minecraft:frog"; }
 	
-	protected function getInitialSizeInfo() : EntitySizeInfo{
+	protected function getInitialSizeInfo() : EntitySizeInfo {
 		return new EntitySizeInfo(0.55, 0.5);
 	}
 
-	public function initEntity(CompoundTag $nbt) : void{
-		$this->setMaxHealth(24);
+	public function initEntity(CompoundTag $nbt) : void {
 		parent::initEntity($nbt);
-		$this->setNameTag($this->nametag." L.".$this->lvl);
+		$this->setMaxHealth(10);
 		$this->setNameTagVisible(true);
 		$this->setNameTagAlwaysVisible(true);
 	}
 
-	public function getName() : string{
+	public function getName() : string {
 		return "Frog";
 	}
 
-	public function getDrops() : array{
+	public function getDrops() : array {
 		return $this->inventory;
 	}
 
-	public function getXpDropAmount() : int{
+	public function getXpDropAmount() : int {
 		return 0;
 	}
 
@@ -54,16 +58,28 @@ class FrogEntity extends Living{
 		if(!$this->isAlive()){
 		  return $hasUpdate;
 		}
-		$playerLocation = $this->getPlayer()->getLocation();
+		
+		if(($item = $this->findItem()) instanceof ItemEntity){
+		  $this->target = $item;
+		}
+		
+		if($this->isAttacking and $this->target instanceof Entity){
+		  $targetLocation = $this->target->getLocation();
+		  if(mt_rand(1, 3) == 1){
+		    $this->attackEntity($this->target);
+		  }
+		} else {
+		  $targetLocation = $this->getPlayer()->getLocation();
+		}
 		$entityLocation = $this->getLocation();
 		if($this->moving){
 		  $inWater = $this->isUnderwater();
-			$this->setHasGravity(!$inWater);
-			$x = $playerLocation->x - $entityLocation->x;
-			$z = $playerLocation->z - $entityLocation->z;
+			$this->setSwimming($inWater);
+			$x = $targetLocation->x - $entityLocation->x;
+			$z = $targetLocation->z - $entityLocation->z;
 			$diff = abs($x) + abs($z);
 			if($diff != 0){
-			  $this->lookAt($playerLocation->add(0, 1, 0));
+			  $this->lookAt($targetLocation->add(0, 1, 0));
 			  if($entityLocation->getWorld()->getBlock($entityLocation->addVector($this->getDirectionVector()))->isSolid()){
 			    $this->jump();
 			  }
@@ -72,13 +88,13 @@ class FrogEntity extends Living{
 				$this->motion->z += $z * $motion;
 			}
 			
-			if($playerLocation->distance($entityLocation) <= 2){
+			if($targetLocation->distance($entityLocation) <= 3 and !$this->target instanceof Entity){
 				$this->moving = false;
 			}
 		} else {
-			if($playerLocation->distance($entityLocation) >= 2){
-			  if($playerLocation->distance($entityLocation) >= 20){
-			    $this->teleport($playerLocation);
+			if($targetLocation->distance($entityLocation) >= 3){
+			  if($targetLocation->distance($entityLocation) >= 20){
+			    $this->teleport($targetLocation);
 			  }
 			  $this->moving = true;
 			}
@@ -86,28 +102,52 @@ class FrogEntity extends Living{
 		return $hasUpdate;
 	}
 
-	protected function onDeath() : void{
+	protected function onDeath() : void {
 		parent::onDeath();
 		FrogForm::setOption($this->getPlayer(), "isSpawned", false);
 		Frog::sendToast($this->getPlayer(), "Frog", "Your frog is dead");
 		$this->flagForDespawn();
 	}
 	
-	public function getSpeed() : float{
+	public function getSpeed() : float {
 		return $this->speed;
 	}
 	
-	public function updateNameTag(string $nametag) : void {
-	  $this->nametag = $nametag;
-	  $this->setNameTag($this->nametag." L.".$this->lvl);
+	public function updateFrog() : void {
+	  $this->nametag = FrogForm::getOptions($this->getPlayer())["nametag"];
+		$this->isAttacking = (bool) FrogForm::getOptions($this->getPlayer())["isAttack"];
+		$this->lvl = (int) FrogForm::getOptions($this->getPlayer())["lvl"];
+	  $this->setNameTag($this->nametag." L. ".$this->lvl);
 	}
 
-	public function setPlayer(?Player $player) : void{
+	public function setPlayer(?Player $player) : void {
 		$this->player = $player;
 	}
 
-	public function getPlayer() : ?Player{
+	public function getPlayer() : ?Player {
 		return $this->player;
+	}
+	
+	public function attack(EntityDamageEvent $source) : void {
+	  parent::attack($source);
+	  if($source instanceof EntityDamageByEntityEvent and $this->isAttacking){
+	    $this->attackEntity($source->getDamager());
+	    $this->target = $source->getDamager();
+	  }
+	}
+	
+	public function attackEntity(Entity $damager) : void {
+	  if($damager->isAlive() and $damager->getLocation()->distance($this->getLocation()) <= 1){
+	    $ev = new EntityDamageByEntityEvent($this, $damager, EntityDamageEvent::CAUSE_ENTITY_ATTACK, 1);
+	    $damager->attack($ev);
+	  } else if(!$damager->isAlive() or $damager->getLocation()->distance($this->getLocation()) >= 15){
+	    $this->target = null;
+	  }
+	}
+	
+	// TODO : LVL
+	public function getLvl() : int {
+	  return $this->lvl;
 	}
 
 }
--- src/mrholler/frog/FrogForm.php
+++ src/mrholler/frog/FrogForm.php
@@ -4,6 +4,9 @@ namespace mrholler\frog;
 
 use pocketmine\player\Player;
 use mrholler\frog\libs\xenialdan\customui\windows\SimpleForm;
+use mrholler\frog\libs\xenialdan\customui\windows\CustomForm;
+use mrholler\frog\libs\xenialdan\customui\elements\Button;
+use mrholler\frog\libs\xenialdan\customui\elements\Toggle;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\DoubleTag;
 use pocketmine\nbt\tag\FloatTag;
@@ -15,21 +18,20 @@ class FrogForm {
   public static function open(Player $player) : void {
     $form = new SimpleForm("Menu Frog");
     // TODO : RENAME FROG
-    // TODO : SWITCH ATTACKING FROG
     // TODO : UP LVL FROG
-    $form->addButtonEasy(self::getOptions($player)["isSpawned"] ? "Despawn" : "Spawn");
+    $form->addButtonEasy("Setting");
+    if(self::getOptions($player)["isSpawned"] and self::findFrog($player) instanceof FrogEntity){
+      $form->addButtonEasy("Despawn");
+    } else {
+      $form->addButtonEasy("Spawn");
+    }
     $form->setCallable(function(Player $player, $data) : void {
       if($data == "Despawn"){
-        $world = $player->getWorld();
-        foreach($world->getEntities() as $entity){
-          if($entity instanceof FrogEntity){
-            if($entity->getPlayer()->getName() == $player->getName()){
-              $entity->flagForDespawn();
-              Frog::sendToast($player, "Frog", "Despawned");
-              self::setOption($player, "isSpawned", false);
-              return;
-            }
-          }
+        $entity = self::findFrog($player);
+        if($entity instanceof FrogEntity){
+          $entity->flagForDespawn();
+          Frog::sendToast($player, "Frog", "Despawned");
+          self::setOption($player, "isSpawned", false);
         }
       }
       if($data == "Spawn"){
@@ -52,12 +54,39 @@ class FrogForm {
             ]));
 	      $frog = new FrogEntity(EntityDataHelper::parseLocation($nbt, $location->getWorld()), $nbt);
         $frog->setPlayer($player);
-        $frog->updateNameTag($options["nametag"]);
+        $frog->updateFrog();
         $frog->spawnToAll();
         Frog::sendToast($player, "Frog", "Spawned");
         self::setOption($player, "isSpawned", true);
         return;
       }
+      if($data == "Setting"){
+        self::openSetting($player);
+      }
+    });
+    $player->sendForm($form);
+  }
+  
+  public static function openSetting(Player $player) : void {
+    $form = new CustomForm("Setting frog");
+    //$form->addInput(self::getOptions($player)["nametag"], "Name frog", self::getOptions($player)["nametag"]);
+    $form->addToggle("Attack", self::getOptions($player)["isAttack"]);
+    $form->setCallable(function(Player $player, $data) : void {
+      /*
+      TODO :(
+      if(isset($data[0])){
+        if(!empty($data[0]) and strlen($data[1]) >= 3){
+          self::setOption($player, "nametag", $data[0]);
+        }
+      }*/
+      if(isset($data[0])){ // 1
+        self::setOption($player, "isAttack", $data[0]); // 1
+      }
+      $entity = self::findFrog($player);
+      if($entity instanceof FrogEntity){
+        $entity->updateFrog();
+      }
+      self::open($player);
     });
     $player->sendForm($form);
   }
@@ -76,6 +105,17 @@ class FrogForm {
     Frog::$config->setNested($player->getName().".".$name, $value);
     Frog::$config->save();
     Frog::$config->reload();
+  }
+  
+  public static function findFrog(Player $player) : ?FrogEntity {
+    foreach($player->getWorld()->getEntities() as $entity){
+      if($entity instanceof FrogEntity){
+        if($entity->getPlayer()->getName() == $player->getName()){
+          return $entity;
+        }
+      }
+    }
+    return null;
   }
   
 }
